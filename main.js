4c9ddaa218f7ca186d8299959db64beeaf8c5ce3
Jonathan Schweder <jonathanschweder@gmail.com>
2021-04-26 21:12:02 +0100

    Fix API endpoint

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var app = new Vue({
             return;
           }
 
-          search_url = 'https://2ansk0ayt1.execute-api.eu-west-1.amazonaws.com/search?number_bedrooms='+vm.number_bedrooms+'&number_bathrooms='+vm.number_bathrooms
+          search_url = 'https://2i73fpnf66.execute-api.eu-west-1.amazonaws.com/search?number_bedrooms='+vm.number_bedrooms+'&number_bathrooms='+vm.number_bathrooms
           axios.get(search_url)
               .then(function(response) {
                 vm.offers = response.data.offers;
